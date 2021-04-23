eee05d03ec501574dae27af4890a028e48b3f691
hlasco <hugues.lascombesdelaroussilhe@uzh.ch>
2021-04-23 15:41:47 +0200

    bugfix

--- exercise_session_06/solution/poisson_solver/jacobi.cpp
+++ exercise_session_06/solution/poisson_solver/jacobi.cpp
@@ -35,13 +35,15 @@ double norm_diff(params p, double** mat1, double** mat2){
 void jacobi_step(params p, double** u_new, double** u_old, double** f){
     double dx = 1.0/((double)p.nx - 1);
     double dy = 1.0/((double)p.ny - 1);
-    double temp;
+
+    for (int i=0; i<p.nx; i++){
+        for (int j=0; j<p.ny; j++)
+            u_old[i][j] = u_new[i][j];
+    }
+
     for (int i=1; i<p.nx-1; i++){
-        for (int j=1; j<p.ny-1; j++){
-            temp = u_new[i][j];
+        for (int j=1; j<p.ny-1; j++)
             u_new[i][j] = 0.25*(u_old[i-1][j] + u_old[i+1][j] + u_old[i][j-1] + u_old[i][j+1] - dx*dy*f[i][j]);
-            u_old[i][j] = temp;
-        }
     }
     // printf("Function jacobi_step (jacobi.cpp l.26): not implemented.\n");
     if (p.nx!=p.ny) printf("In function jacobi_step (jacobi.cpp l.26): nx != ny, check jacobi updates\n");

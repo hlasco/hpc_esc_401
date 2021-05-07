c962b520e9ccbeb56d8e4330adeea0038fb8fb51
jbucko <jbucko@ethz.ch>
2021-05-07 12:21:50 +0200

    exercise_session_09

---
+++ exercise_session_09/mapper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+import sys
+import string
+
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace and punctuation
+    # line = line.strip().translate(None, string.punctuation).lower()
+    line = line.strip().translate(str.maketrans('','',string.punctuation)).lower()
+    # split the line into words
+    words = line.split()
+    # increase counters
+    for word in words:
+        # write the results to STDOUT (standard output);
+        # what we output here will be the input for the
+        # Reduce step, i.e. the input for reducer.py
+        #
+        # tab-delimited; the trivial word count is 1
+        print( '%s\t%s' % (word, 1))
